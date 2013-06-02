ec4685148a4e1f6b766325e687ac6a261c99d203
Carey Zhang <carey.zhang@gmail.com>
2013-06-02 01:17:08 -0700

    add graphics scene class, black background in scene

---
+++ blobertscene.cpp
@@ -0,0 +1,7 @@
+#include "blobertscene.h"
+
+BlobertScene::BlobertScene(QObject *parent) :
+    QGraphicsScene(parent)
+{
+    setBackgroundBrush(QBrush(QColor(0,0,0)));
+}
---
+++ blobertscene.h
@@ -0,0 +1,18 @@
+#ifndef BLOBERTSCENE_H
+#define BLOBERTSCENE_H
+
+#include <QGraphicsScene>
+
+class BlobertScene : public QGraphicsScene
+{
+    Q_OBJECT
+public:
+    explicit BlobertScene(QObject *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // BLOBERTSCENE_H
